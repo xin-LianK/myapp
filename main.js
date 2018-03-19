ca1d162e3f836b190ddb9a77fd03d0a77e666a9f
xin-LianK <350824110@qq.com>
2018-03-19 16:03:49 +0800

    阻塞与非阻塞调用的不同
    🍔🍔🍔第一个实例在文件读取完后才执行完程序。 第二个实例我们不需要等待文件读取完，这样就可以在读取文件时同时执行接下来的代码，大大提高了程序的性能

---
+++ main.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+// 阻塞代码实例
+
+// var data = fs.readFileSync('input.txt');
+
+// console.log(data.toString());
+// console.log("程序执行结束");
+
+// 非阻塞代码实例
+
+fs.readFile('input.txt',function(err,data){
+    if(err) return console.log(err);
+    console.log(data.toString());
+});
+console.log("程序执行结束");
