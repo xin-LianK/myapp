e860ef095b761d7448318a9f5e394a9e6575195e
xin-LianK <350824110@qq.com>
2018-03-19 16:19:40 +0800

    事件循环

--- main.js
+++ main.js
@@ -9,8 +9,36 @@ var fs = require("fs");
 
 // 非阻塞代码实例
 
-fs.readFile('input.txt',function(err,data){
-    if(err) return console.log(err);
-    console.log(data.toString());
+// fs.readFile('input.txt', function (err, data) {
+//     if (err) return console.log(err);
+//     console.log(data.toString());
+// });
+// console.log("程序执行结束");
+
+
+
+// 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+
+// 创建事件处理程序
+var connectHandler = function connected() {
+    console.log('连接成功。');
+
+    // 触发 data_received 事件 
+    eventEmitter.emit('data_received');
+}
+
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', connectHandler);
+
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function () {
+    console.log('数据接收成功。');
 });
-console.log("程序执行结束");
+
+// 触发 connection 事件 
+eventEmitter.emit('connection');
+
+console.log("程序执行完毕。");
